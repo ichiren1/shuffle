56dc12420e47b370c2cb35bdf9768dbac90960cd
ichiren1 <ichiren1@gmail.com>
2022-04-15 00:51:31 +0900

    routerを使う&format

--- backend/src/handlers/root.ts
+++ backend/src/handlers/root.ts
@@ -1,5 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
-export const rootHandler = (_req: Request, res: Response) => {
-  return res.send('Hello, ichiren shuffle API is awesome');
-};
+export const rootHandler = (_req: Request, res: Response) =>
+  res.send("Hello, ichiren shuffle API is awesome");
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import { rootHandler } from './handlers/root';
-import { chooseOneHandler, shuffleHandler } from './handlers/shuffle';
+import { router } from './router';
 
 const app = express();
 const port = process.env.PORT || '8000';
-
 app.get('/', rootHandler);
-app.get('/v1/shuffle/shuffle', shuffleHandler);
-app.get('/v1/shuffle/chooseOne', chooseOneHandler);
+app.use('/v1', router);
 
-app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`);
-});
+app.listen(port, () => console.log(`Server is listening on ${port}`));
