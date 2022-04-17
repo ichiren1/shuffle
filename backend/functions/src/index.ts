3dccb008b48cc1752613e931b50bde6cb3ffed4a
ichiren1 <ichiren1@gmail.com>
2022-04-17 10:15:11 +0900

    コミット忘れ #1 の続き

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,8 +1,10 @@
 import * as functions from "firebase-functions";
 import express from "express";
+import cors from "cors";
 import { router } from "./router";
 
 const app = express();
+app.use(cors({ origin: true }));
 
 app.use("/v1", router);
 
