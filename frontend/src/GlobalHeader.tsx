0d10ec8da49b3e31efdd01bfc8b510f513e236cc
ichiren1 <ichiren1@gmail.com>
2022-05-04 22:53:09 +0900

    ヘッダーを追加

---
+++ frontend/src/GlobalHeader.tsx
@@ -0,0 +1,9 @@
+import './GlobalHeader.css'
+
+export function GlobalHeader() {
+  return (
+    <div className='GlobalHeader'>
+      <h1 className='GlobalHeader-title'>便利なシャッフル</h1>
+    </div>
+  )
+}
