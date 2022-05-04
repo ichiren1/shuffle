9a159aadddf1d874ff779ebe4795ed3718b8fbb8
ichiren1 <ichiren1@gmail.com>
2022-05-04 22:54:33 +0900

    シャッフルの結果部分のデザイン修正

--- frontend/src/features/shuffle/ShuffleResultWrapper.tsx
+++ frontend/src/features/shuffle/ShuffleResultWrapper.tsx
@@ -2,19 +2,24 @@ import { ShuffleResult as ShuffleResultComponent } from './ShuffleResult'
 import { ChooseOneResult as ChooseOneResultComponent } from './ChooseOneResult'
 import { RpsResult as RpsResultComponent } from './RpsResult'
 import { ChooseOneResult, RpsResult, ShuffleResult } from './request'
+import './ShuffleResultWrapper.css';
 
 export function ShuffleResultWrapper(props: ShuffleResultWrapperPropType) {
   
   return (
-    <div>
-      {(props.results.type === 'Shuffle') && <ShuffleResultComponent candidates={props.candidates} result={props.results.result} type={'Shuffle'} />}
-      {(props.results.type === 'ChooseOne') && <ChooseOneResultComponent candidates={props.candidates} result={props.results.result} type={'ChooseOne'} />}
-      {(props.results.type === 'Rps') && <RpsResultComponent candidates={props.candidates} type={'Rps'} result={props.results.result} state={props.results.state} /> }
+    <div className='ShuffleResultWrapper'>
+      <h2 className='ShuffleResultWrapper-title'>結果</h2>
+      {!props.results && <div id="ShuffleResultWrapper-results-empty-message">候補者を追加し、「シャッフル」「1つ選ぶ」「じゃんけん」のどれかを選んでください。</div>}
+      {props.results && (<div>
+        {(props.results.type === 'Shuffle') && <ShuffleResultComponent candidates={props.candidates} result={props.results.result} type={'Shuffle'} />}
+        {(props.results.type === 'ChooseOne') && <ChooseOneResultComponent candidates={props.candidates} result={props.results.result} type={'ChooseOne'} />}
+        {(props.results.type === 'Rps') && <RpsResultComponent candidates={props.candidates} type={'Rps'} result={props.results.result} state={props.results.state} />}
+      </div>)}
     </div>
   )
 }
 
 export type ShuffleResultWrapperPropType = {
   candidates: string[],
-  results: ShuffleResult | ChooseOneResult | RpsResult
+  results: ShuffleResult | ChooseOneResult | RpsResult | null
 }
